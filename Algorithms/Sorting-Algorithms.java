bb4bcb7c56ac2f90fdf2015a211539b34b8bf802
Ryan Lee <96277496+RyunKL@users.noreply.github.com>
2023-03-12 00:26:24 -0500

    Update Sorting-Algorithms.java

--- Algorithms/Sorting-Algorithms.java
+++ Algorithms/Sorting-Algorithms.java
@@ -27,17 +27,126 @@ public class Main {
             }
         }
     }
+     /**
+     * NAME: Insertion Sort
+     *  - after comparing elements to the left shift elements to the right to make room to insert a value
+     * RUN TIME COMPLEXITY: O(n^2) = Quadratic Time
+     *  - the larger the data size the more in-efficient it is to use
+     *  - fewer steps than bubbleSort
+     *  - best case is O(n) compared to Selection Sort O(n^2)
+     *  - small data = decent | big data = BAD
+     */
     public static void insertionSort(int[] array){
-        //Code here
+        for(int i = 0; i < array.length; i++){
+            int temp = array[i];
+            int j = i - 1;
+            while(j >= 0 && array[j] > temp){
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
     }
+     /**
+     * NAME: Bubble Sort
+     *  - pairs of adjacent elements are compared,
+     *  and the elements swapped if they are not in order
+     * RUN TIME COMPLEXITY: O(n^2) = Quadratic Time
+     *  - the larger the data size the more in-efficient it is to use
+     *  - small data = decent | big data = BAD
+     */
     public static void bubbleSort(int[] array){
-        //Code here
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length - 1; j++){
+                if(array[j] > array[j + 1]){
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
     }
+    //Uses recursion divide and conquer technique
+    public static void quickSort(int[] array, int start, int end){
+        if(end <= start) return;
+        int pivot = partition(array, start, end);
+        quickSort(array, start, pivot - 1);
+        quickSort(array, pivot + 1, end);
+    }
+    private static int partition(int[] array, int start, int end){
+        int pivot = array[end];
+        int i = start - 1;
+
+        for(int j = start; j <= end - 1; j++){
+            if(array[j] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        i++;
+        int temp = array[i];
+        array[i] = array[end];
+        array[end] = temp;
+        return i;
+    }
+    //MergeSort = recursively divide array into 2, sorts, re-combine
+    //run-time complexity = 0(log n) - quasilinear time (looks quadratic to me)
+    //                               - similar run time of quicksort, heapsort
+    //                               - faster than: insertion, selection, and bubble sort
+    //space complexity = 0(n) - linear space
     public static void mergeSort(int[] array){
-        //Code here
+        int length = array.length;
+        if(length <= 1) return;
+
+        int middle = length/2;
+        int [] left = new int[middle];
+        int [] right = new int[length - middle];
+
+        int i = 0;// left array
+        int j = 0;// right array
+
+        for(; i < length; i++){
+            if(i < middle){
+                left[i] = array[i];
+            }else{
+                right[j] = array[i];
+                j++;
+            }
+        }
+        mergeSort(left);
+        mergeSort(right);
+        merge(left, right, array);
     }
-    public static void merge(int[] left, int[] right, int[] array){
+    private static void merge(int[] left, int[] right, int[] array){
         //Sub function of the mergeSort
+        int leftLength = left.length/2;
+        int rightLength = right.length - leftLength;
+        int i = 0, l = 0, r = 0; // indices
+
+        //check conditions for merge
+        while(l < leftLength && r < rightLength){
+            if(left[l] < right[r]){
+                array[i] = left[l];
+                i++;
+                l++;
+            }else{
+                array[i] = right[r];
+                i++;
+                r++;
+            }
+        }
+        while(l < leftLength){
+            array[i] = left[l];
+            i++;
+            l++;
+        }
+        while(r < rightLength){
+            array[i] = right[r];
+            i++;
+            r++;
+        }
     }
     public static void main(String[] args) {
         //Creating an unorganized array to be sorted
@@ -47,23 +156,36 @@ public class Main {
         int[] array2 = new int[20];
         int[] array3 = new int[20];
         int[] array4 = new int[20];
+        int[] array5 = new int[20];
 
         for(int i = 0; i < array.length; i++){
             array[i] = rand.nextInt(100);
             array2[i] = rand.nextInt(100);
             array3[i] = rand.nextInt(100);
             array4[i] = rand.nextInt(100);
+            array5[i] = rand.nextInt(100);
         }
         //Printing unorganized Arrays
+        System.out.println("-------------------------UnOrdered Arrays-------------------------");
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(array2));
         System.out.println(Arrays.toString(array3));
         System.out.println(Arrays.toString(array4));
+        System.out.println(Arrays.toString(array5));
 
         //Calling on each Sorting Algorithm
         selectionSort(array);
+        insertionSort(array2);
+        bubbleSort(array3);
+        mergeSort(array4);
+        quickSort(array5, 0, 19);
 
         //Printing Organized Arrays Ascending
+        System.out.println("\n-------------------------Organized Ascending Arrays-------------------------")
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(array));
     }
 }
